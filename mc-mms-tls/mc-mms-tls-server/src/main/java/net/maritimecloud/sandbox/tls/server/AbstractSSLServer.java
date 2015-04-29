7d8c5becf914aae35d21a084efe6e8dbf60db4fe
Kasper Nielsen <kasperni@gmail.com>
2015-04-29 12:54:11 +0200

    update

---
+++ mc-mms-tls/mc-mms-tls-server/src/main/java/net/maritimecloud/sandbox/tls/server/AbstractSSLServer.java
@@ -0,0 +1,85 @@
+/* Copyright (c) 2011 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.maritimecloud.sandbox.tls.server;
+
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+/**
+ *
+ * @author Kasper Nielsen
+ */
+public class AbstractSSLServer {
+
+    public void start(String[] args) throws Exception {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        // HTTP Configuration
+        HttpConfiguration http_config = new HttpConfiguration();
+        http_config.setSecureScheme("https");
+        http_config.setSecurePort(8443);
+        http_config.setOutputBufferSize(32768);
+
+        // SSL Context Factory for HTTPS
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath("src/main/resources/server-keystore.jks");
+        sslContextFactory.setKeyStorePassword("changeit");
+        sslContextFactory.setKeyManagerPassword("changeit");
+
+        configure(sslContextFactory);
+
+        // HTTPS Configuration
+        HttpConfiguration https_config = new HttpConfiguration(http_config);
+        https_config.addCustomizer(new SecureRequestCustomizer());
+
+        // HTTPS connector
+        ServerConnector https = new ServerConnector(server, new SslConnectionFactory(sslContextFactory,
+                HttpVersion.HTTP_1_1.asString()), new HttpConnectionFactory(https_config));
+        https.setPort(8443);
+        https.setIdleTimeout(500000);
+        server.addConnector(https);
+
+        // Setup the basic application "context" for this application at "/"
+        // This is also known as the handler tree (in jetty speak)
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        // Initialize javax.websocket layer
+        ServerContainer wscontainer = WebSocketServerContainerInitializer.configureContext(context);
+
+        // Add WebSocket endpoint to javax.websocket layer
+        wscontainer.addEndpoint(SimpleEndpoint.class);
+
+        server.start();
+        // server.dump(System.err);
+        System.out.println("RUNNING");
+        server.join();
+    }
+
+    protected void configure(SslContextFactory factory) {}
+}
---
+++ mc-mms-tls/mc-mms-tls-server/src/main/java/net/maritimecloud/sandbox/tls/server/SSLServerWithClientCertificateValidation.java
@@ -0,0 +1,37 @@
+/* Copyright (c) 2011 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.maritimecloud.sandbox.tls.server;
+
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+/**
+ *
+ * @author Kasper Nielsen
+ */
+public class SSLServerWithClientCertificateValidation extends AbstractSSLServer {
+
+    public static void main(String[] args) throws Exception {
+        new SSLServerWithClientCertificateValidation().start(args);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void configure(SslContextFactory sslContextFactory) {
+        sslContextFactory.setNeedClientAuth(true);
+        sslContextFactory.setTrustStorePath("src/main/resources/server-truststore.jks");
+        sslContextFactory.setTrustStorePassword("changeit");
+    }
+
+}
