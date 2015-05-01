258fc1350e71cda9fde645cb0f63dbe7f69534e8
peder280370 <peder@carolus.dk>
2015-05-01 16:04:47 +0200

    Let server endpoint print out identity of SSL client certificate

--- mc-mms-tls/mc-mms-tls-server/src/main/java/net/maritimecloud/sandbox/tls/server/util/SimpleEndpoint.java
+++ mc-mms-tls/mc-mms-tls-server/src/main/java/net/maritimecloud/sandbox/tls/server/util/SimpleEndpoint.java
@@ -1,6 +1,7 @@
 package net.maritimecloud.sandbox.tls.server.util;
 
-import java.io.IOException;
+import org.eclipse.jetty.websocket.common.WebSocketSession;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
 
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
@@ -9,16 +10,35 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.security.Principal;
+import java.security.cert.X509Certificate;
 
 @ServerEndpoint(value = "/websockettest/")
 public class SimpleEndpoint {
 
     Session sess;
+    Principal principal;
 
     @OnOpen
     public void onWebSocketConnect(Session sess) {
         System.out.println("Socket Connected: ");
         this.sess = sess;
+
+        try {
+            // TODO: Is there a non-jetty way of doing this?
+            WebSocketSession jettySession = (WebSocketSession)sess;
+            ServletUpgradeRequest upgradeRequest = (ServletUpgradeRequest)jettySession.getUpgradeRequest();
+            X509Certificate[] certs = upgradeRequest.getCertificates();
+            if (certs != null && certs.length > 0) {
+                X509Certificate cert = certs[0];
+                principal = cert.getSubjectDN();
+                System.out.println("Client Connected with trusted SSL certificate: " + principal);
+            }
+        } catch (Exception e) {
+            System.err.println("Error inspecting client certificates " + e);
+        }
+
     }
 
     @OnMessage
